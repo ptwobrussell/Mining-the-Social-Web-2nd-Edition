53707c8a950833521448860dd4db26a75d45f499
Mario Rodas <rodasmario2@gmail.com>
2013-08-11 20:35:15 -0500

    Remove vagrant-berkshelf as dependency

--- deploy/cookbooks/mtsw2e/metadata.rb
+++ deploy/cookbooks/mtsw2e/metadata.rb
@@ -5,9 +5,8 @@ license          "MIT"
 description      "Installs/Configures mtsw2e"
 version          "0.2.0"
 
-depends "apt"
-depends "runit"
-depends "python"
-depends "build-essential"
+depends "apt", "~> 2.0.0"
+depends "runit", "<= 1.0.4"
+depends "python", "~> 1.3.6"
 
 supports "ubuntu"
